f6bde336919602106327b89e13873ead4137740e
jmverrelli <jmverrelli@localhost.localdomain>
2016-10-12 09:06:34 -0300

    Agrego el gitignore

--- data/conexion/conectionData.php
+++
@@ -1,8 +0,0 @@
-<?php
-//TODO: cambiar por la conexion real
-//define("HOSTING", '192.168.253.102');
-define("HOST", 'localhost');
-
-define("DBvieja", 'contrataciones');
-define("USRDBvieja", 'usuario');
-define("PASSDBvieja", 'password');
