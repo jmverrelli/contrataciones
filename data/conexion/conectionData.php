bfe5357823cca0a0ab8d3c6aa32fa7126e71fc8e
jmverrelli <jmverrelli@users.noreply.github.com>
2016-10-12 08:59:10 -0300

    Delete conectionData.php

--- data/conexion/conectionData.php
+++
@@ -1,8 +0,0 @@
-<?php
-//TODO: cambiar por la conexion real
-//define("HOSTING", '192.168.253.102');
-define("HOST", 'localhost');
-
-define("DBvieja", 'contrataciones');
-define("USRDBvieja", 'root');
-define("PASSDBvieja", 's20s05');
